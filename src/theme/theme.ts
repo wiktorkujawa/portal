8c4dc24dce6551ffaed3f64f7b8715340b046ed6
wiktor <wiktorkujawa1993@gmail.com>
2021-06-11 14:32:12 +0200

    first commit

---
+++ src/theme/theme.ts
@@ -0,0 +1,10 @@
+export const theme = {
+  colors:
+  {
+    background: '#FFFFFF',
+    font: '#212121',
+    disabledButton: '#E2E2E2',
+    activeButton: '#4B4B4B',
+    border:'#C6C6C6'
+  }
+};
